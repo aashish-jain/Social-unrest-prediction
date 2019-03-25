d972feb7d55b8efb9daa23387eafa126661a767c
ateexd <ateendraramesh@gmail.com>
2019-03-25 17:29:38 -0400

    something we can use

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -7,9 +7,9 @@ from collections import Counter
 def get_glove_dict():
     """
     Inputs - None
-    
+
     Returns - Dict below
-    
+
     Create a dict with word as key and word vector as value
     """
     d = {}
@@ -24,18 +24,18 @@ def get_glove_dict():
 
 def generate_labels(num_events, num_days=2, days_window=2):
     """
-    Inputs - 
-        num_events - as generated as above 
-        num_days - the number of days to after which we 
+    Inputs -
+        num_events - as generated as above
+        num_days - the number of days to after which we
                    look for the occurence of an event.
                    Basically, the future
         day_window - If an event occurs in a window of days
                      after num_days
-    
+
     Returns - Returns labels generated as below
-    
+
     Generate labels by using the future events, basically
-    look-ahead to create labels. If an event occurs from 
+    look-ahead to create labels. If an event occurs from
     i to i + num_days, then label is 1.0 else 0.0
     """
     n = len(num_events)
@@ -47,7 +47,7 @@ def generate_labels(num_events, num_days=2, days_window=2):
 def get_cities(df, top_locations):
     """
     Input - DF from reading ACLED CSV
-    Returns - String of cities requested based on 
+    Returns - String of cities requested based on
               top_locations in process_acled_csv(..)
     """
     counter = Counter(df.location)
@@ -55,17 +55,17 @@ def get_cities(df, top_locations):
         return list(counter.keys())
     counter = sorted(counter.items(), key=lambda x: -x[1])[:top_locations]
     return [x[0] for x in counter]
-    
+
 
 def process_acled_csv(path_to_csv, top_locations=10, lead_days=2, days_window=5):
     """
-    Input - 
+    Input -
         path_to_csv - The path to the CSV file and returns labels
         top_locations - Number of top cities that generated ACLED articles
                         if top_locations is -1, returns for all cities
                         Default value = 10
         lead_days - Lead days before which a prediction should be made
-        days_window - The number of days after lead_days that we are 
+        days_window - The number of days after lead_days that we are
                       concerned about
 
     Returns - Labels for each location and each of the dates in the CSV
@@ -74,12 +74,12 @@ def process_acled_csv(path_to_csv, top_locations=10, lead_days=2, days_window=5)
         df = pd.read_csv(path_to_csv)
     except:
         raise FileNotFoundError
-    
+
     df['event_date'] = pd.to_datetime(df['event_date'])
     df = df[df['event_date'] >= pd.to_datetime("23-feb-2019")]
-    
+
     df.sort_values(by=['event_date'], inplace=True)
-    
+
     start, end = df.iloc[0]['event_date'], df.iloc[-1]['event_date']
     dates = pd.date_range(start=start, end=end)
     print("Data from", start, "to", end, " & Number of days -", len(dates))
@@ -101,7 +101,7 @@ def process_acled_csv(path_to_csv, top_locations=10, lead_days=2, days_window=5)
 
 def plot_counter(arr, num_elements=10, reverse=True, xlabel="", ylabel="", title=""):
     """
-    Input - 
+    Input -
         arr - This could be an list/np.ndarray/pd.Series etc
         num_elements - Number of elements needed to be plotted,
                        default value = 10
@@ -114,10 +114,10 @@ def plot_counter(arr, num_elements=10, reverse=True, xlabel="", ylabel="", title
     """
     # Get counter
     counter = Counter(arr)
-    
+
     # Sort as per criterion given in function definition
     counter = sorted(counter.items(), key=lambda x: x[1], reverse=reverse)[:num_elements]
-    
+
     counter = np.array(counter)
 
     things , counts = counter[:, 0].tolist(), counter[:, 1].astype(np.float32)
@@ -131,7 +131,5 @@ def plot_counter(arr, num_elements=10, reverse=True, xlabel="", ylabel="", title
     plt.xticks(indices, things)
     plt.title(title)
 
-    # Matplotlib magic
-    plt.rcParams['figure.figsize'] = (20, 10)
     plt.rcParams['figure.figsize'] = (20, 10)
     _ = plt.bar(indices, counts, 0.5)
