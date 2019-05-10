fcca47ed4812e4ef72810d4168a27cfa5311ac78
Daniel Amirtharaj <dani.amirtharaj@gmail.com>
2019-05-10 11:55:28 -0400

    Updated UI

--- angular_app/violence-pred/src/app/date-location-pred/date-location-pred.component.ts
+++ angular_app/violence-pred/src/app/date-location-pred/date-location-pred.component.ts
@@ -21,20 +21,24 @@ export class DateLocationPredComponent implements OnInit {
     this.getLocations().subscribe((data : string[]) => this.locations = data['locations']);
   }
   
-  maxDate = new Date(2020, 4, 0);
-  minDate = new Date(2016, 1, 0);
-  date = new Date(2020, 1, 0);
+  maxDate = new Date(2019, 4, 3);
+  minDate = new Date(2019, 1, 23);
+  date = new Date(2019, 4, 3);
 
   configUrl = 'http://127.0.0.1:5000/predict/';
 
   locationValue: string;
   dateString : string;
+  date1String : string;
+  date2String : string;
   result : number;
   resultString : string;
   disp : boolean;
 
   onClickGo() {
     this.dateString = this.date.getMonth()+1+"/"+this.date.getDate()+"/"+this.date.getFullYear();
+    this.date1String = this.date.getMonth()+1+"/"+(this.date.getDate()+2)+"/"+this.date.getFullYear();
+    this.date2String = this.date.getMonth()+1+"/"+(this.date.getDate()+7)+"/"+this.date.getFullYear();
     this.getConfig().subscribe((data : Result) => this.result = data['probability']);
     this.disp = true;
   }
